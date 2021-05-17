9f4caf014cb1be2699f0b9ddbda62443fa160e68
flaviofz <flaviofz@gmail.com>
2021-05-17 06:31:16 -0300

    Upload de imagem do produto

--- src/DevIO.App/Controllers/ProdutosController.cs
+++ src/DevIO.App/Controllers/ProdutosController.cs
@@ -2,9 +2,11 @@
 using DevIO.App.ViewModels;
 using DevIO.Business.Interfaces;
 using DevIO.Business.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace DevIO.App.Controllers
@@ -50,10 +52,17 @@ namespace DevIO.App.Controllers
 
             if (!ModelState.IsValid) return View(produtoViewModel);
 
+            var imgPrefixo = Guid.NewGuid() + "_";
+            if (! await UploadArquivo(produtoViewModel.ImagemUpload, imgPrefixo))
+            {
+                return View(produtoViewModel);
+            }
+
+            produtoViewModel.Imagem = imgPrefixo + produtoViewModel.ImagemUpload.FileName;
             await _produtoRepository.Adicionar(_mapper.Map<Produto>(produtoViewModel));
 
-            return View(produtoViewModel);
-        }
+            return RedirectToAction("Index");
+        }        
 
         [Route("editar-produto/{id:guid}")]
         public async Task<IActionResult> Edit(Guid id)
@@ -114,24 +123,24 @@ namespace DevIO.App.Controllers
             return produto;
         }
 
-        //private async Task<bool> UploadArquivo(IFormFile arquivo, string imgPrefixo)
-        //{
-        //    if (arquivo.Length <= 0) return false;
+        private async Task<bool> UploadArquivo(IFormFile arquivo, string imgPrefixo)
+        {
+            if (arquivo.Length <= 0) return false;
 
-        //    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgPrefixo + arquivo.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgPrefixo + arquivo.FileName);
 
-        //    if (System.IO.File.Exists(path))
-        //    {
-        //        ModelState.AddModelError(string.Empty, "Já existe um arquivo com este nome!");
-        //        return false;
-        //    }
+            if (System.IO.File.Exists(path))
+            {
+                ModelState.AddModelError(string.Empty, "Já existe um arquivo com este nome!");
+                return false;
+            }
 
-        //    using (var stream = new FileStream(path, FileMode.Create))
-        //    {
-        //        await arquivo.CopyToAsync(stream);
-        //    }
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
 
-        //    return true;
-        //}
+            return true;
+        }
     }
 }
